94eb1064e77945d8cabc303ac1e77b0c9b4ee0cd
Brett DeWoody <brettdewoody@gmail.com>
2018-01-03 00:27:07 +0100

    Allow user to select a different num of photos.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // Global variables
 const YEAR = 2017;
 const DOMAIN = `${window.location.origin}/`;
+const CANVAS_SIZES = [4, 9, 16, 25];
+const DEFAULT_TAB = 9;
 const HASH = window.location.hash.substr(1).split("=");
 const API_CLIENT_ID = "96553afb3bb9430d91c2d2ee9d8c5c75";
 const API_BASE = "https://api.instagram.com/";
@@ -47,9 +49,26 @@ const renderPics = () => {
   fetchMedia(API_ENDPOINT, [])
     .then(response => {
       createCollage(response).then(response => {
-        (["js-download--1", "js-download--2", "js-download--3"]).forEach(id => enableDownloadLink(id, "js-canvas"));
+        floatCanvas(DEFAULT_TAB);
+        CANVAS_SIZES.forEach(canvasSize => {
+          const canvas = document.getElementById(`js-canvas--${canvasSize}`);
+          canvas.dataset["url"] = canvas.toDataURL();
+        });
+        (["js-download--1", "js-download--2", "js-download--3"]).forEach(id => enableDownloadLink(id, `js-canvas--${DEFAULT_TAB}`));
         renderView("pics");
       });
+
+      document.getElementById(`tab-${DEFAULT_TAB}`).classList.add("active");
+      const tabs = Array.from(document.querySelectorAll(".js-select-pics"));
+      tabs.forEach(tab => {
+        tab.addEventListener("click", event => {
+          const numPics = tab.dataset.pics;
+          document.querySelector(".canvas__tab.active").classList.remove("active");
+          tab.classList.add("active");
+          floatCanvas(numPics);
+          (["js-download--1", "js-download--2", "js-download--3"]).forEach(id => enableDownloadLink(id, `js-canvas--${numPics}`));
+        })
+      })
     })
     .catch(displayError);
 };
@@ -80,7 +99,7 @@ const getRecent = (endpoint, media) => {
       const updatedMedia = media
         .concat(newMedia)
         .sort((a, b) => b.likes.count - a.likes.count || b.comments.count - a.comments.count)
-        .splice(0, 4);
+        .splice(0, 25);
 
       if (moreResults) {
         return getRecent(response.pagination.next_url, updatedMedia);
@@ -91,13 +110,26 @@ const getRecent = (endpoint, media) => {
     .catch(displayError);
 };
 
-const addMedia = (ctx, url, pos, w) => {
+const floatCanvas = id => {
+  const canvases = Array.from(document.querySelectorAll(".canvas"));
+  canvases.forEach(canvas => {
+    if (canvas.getAttribute("id") === `js-canvas--${id}`) {
+      canvas.style.zIndex = 10;
+    } else {
+      canvas.style.zIndex = 0;
+    }
+  });
+
+  document.getElementById(`js-canvas--${id}`).style.zIndex = 10;
+}
+
+const addMedia = (ctx, url, posX, posY, w) => {
   return new Promise((resolve, reject) => {
     const image = new Image();
     image.crossOrigin = "anonymous";
     image.onload = () => {
       const crop = Math.min(image.width, image.height);
-      ctx.drawImage(image, 0, 0, crop, crop, pos[0], pos[1], w, w);
+      ctx.drawImage(image, 0, 0, crop, crop, posX, posY, w, w);
       return resolve(image);
     };
     image.src = url;
@@ -106,32 +138,44 @@ const addMedia = (ctx, url, pos, w) => {
 
 const getMediaYear = date => new Date(date * 1000).getFullYear();
 
-const addText = (ctx, text, pos) => {
+const addText = (ctx, text, pos_x, pos_y) => {
   ctx.textAlign = "left";
   ctx.font = "36px -apple-system, system-ui, Arial";
   ctx.fillStyle = "#222222";
-  ctx.fillText(text, pos[0], pos[1]);
+  ctx.fillText(text, pos_x, pos_y);
 };
 
 const createCollage = media => {
-  return new Promise((resolve, reject) => {
-    const canvas = document.getElementById("js-canvas");
+  const imagePromises = [];
+
+  CANVAS_SIZES.forEach(canvasSize => {
+    const canvas = document.getElementById(`js-canvas--${canvasSize}`);
     const context = canvas.getContext("2d");
-    const positions = [[30, 0], [438, 0], [30, 408], [438, 408]];
-    const numLikes = media.reduce((total, item) => (total += item.likes.count), 0);
-    const imagePromises = [];
+    const numLikes = media.slice(0, canvasSize).reduce((total, item) => (total += item.likes.count), 0);
 
     canvas.width = 868;
     canvas.height = canvas.width;
     context.fillStyle = "#ffffff";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    media.forEach((item, i) => {
-      imagePromises.push(addMedia(context, item.images.standard_resolution.url, positions[i], 400));
-    });
+    const gridNum = Math.sqrt(canvasSize);
+    const gutterWidth = 6;
+    const offset = 30;
+    const imageWidth = ((canvas.width - (offset * 2)) - (gutterWidth * (gridNum - 1)) ) / gridNum;
 
-    addText(context, `My 2017 Top 4 Posts - ${numLikes.toLocaleString()} Likes`, [30, canvas.height - 20]);
+    for (let i = 0; i < canvasSize; i++) {
+      const item = media[i];
+      const col = i % gridNum;
+      const row = Math.floor(i / gridNum);
+      const posX = offset + (imageWidth * col) + (gutterWidth * col);
+      const posY = (imageWidth * row) + (gutterWidth * row);
+      imagePromises.push(addMedia(context, item.images.standard_resolution.url, posX, posY, imageWidth));
+    }
 
+    addText(context, `My 2017 Top ${canvasSize} Posts - ${numLikes.toLocaleString()} Likes`, 30, canvas.height - 20);
+  });
+
+  return new Promise((resolve, reject) => {
     Promise.all(imagePromises).then(responses => {
       resolve(true)
     })
@@ -140,7 +184,7 @@ const createCollage = media => {
 
 const enableDownloadLink = (id, canvasId) => {
   const link = document.getElementById(id);
-  link.href = document.getElementById(canvasId).toDataURL();
+  link.href = document.getElementById(canvasId).dataset.url;
   link.download = "My2017Top4.jpg";
 }
 
